760e284764c855b6f370e2110941672faf523ff4
Doug <dougkpr@gmail.com>
2024-02-28 09:56:20 -0300

    update code and logging output

--- integration/connectors/airbyte.py
+++ integration/connectors/airbyte.py
@@ -2,10 +2,13 @@ import time
 import requests
 import base64
 import logging
+from typing import Literal
 
 
-class AirbyteClient:
-    def __init__(self, server_name: str, username: str, password: str):
+class AirbyteClient(object):
+    FAILED_JOB_STATUS = ["incomplete", "failed", "cancelled"]
+
+    def __init__(self, server_name: str, username: str, password: str) -> bool:
         self.server_name = server_name
         self.username = username
         self.password = password
@@ -18,33 +21,43 @@ class AirbyteClient:
     def valid_connection(self) -> bool:
         """Check if connection is valid"""
         url = f"http://{self.server_name}:8006/health"
-        response = requests.get(url=url, headers=self.headers)
-        
-        if response.status_code == 200:
-            return True
-        else:
-            raise Exception(
-                f"Airbyte connection is not valid. Status code: {response.status_code}. Error message: {response.text}"
-            )
 
-    def trigger_sync(self, connection_id: str):
-        """Trigger sync for a connection_id"""
-        url = f"http://{self.server_name}:8006/v1/jobs"
-        data = {"connectionId": connection_id, "jobType": "sync"}
-        response = requests.post(url=url, json=data, headers=self.headers)
+        try:
+            response = requests.get(url=url, headers=self.headers)
+            response.raise_for_status()
+
+            return True
+        except Exception as e:
+            raise requests.exceptions.ConnectionError(
+                f"Invalid Airbyte connection. Status code: {e.response.status_code}. Error message: {e.response.text}"
+            )
+    
+    def trigger_job(self, connection_id: str, job_type: Literal["reset", "sync"] = "sync", sleep_seconds: int = 5) -> None:
+        """Trigger job (reset or sync) for a connection_id"""
+        post_url = f"http://{self.server_name}:8006/v1/jobs"
+        post_payload = {
+            "connectionId": connection_id,
+            "jobType": job_type
+        }
+
+        response = requests.post(url=post_url, json=post_payload, headers=self.headers)
         job_id = response.json().get("jobId")
-        job_status = response.json().get("status")
+
+        if (job_status := response.json().get("status")) != "running":
+            raise Exception(f"Job {job_id} has failed to start.")
+        
         while job_status == "running":
-            sleep_seconds = 5
             logging.info(
-                f"Job {job_id} is running. Checking job again in {sleep_seconds} seconds."
+                f"Job {job_id} is running. Polling job status again in {sleep_seconds} seconds."
             )
             time.sleep(sleep_seconds)
-            url = f"http://{self.server_name}:8000/api/v1/jobs/get"
-            data = {"id": job_id}
-            job_response = requests.post(url=url, json=data, headers=self.headers)
-            job_status = job_response.json().get("job").get("status")
-            if job_status == "failed":
-                raise Exception(f"Job {job_id} has failed. {job_response.text}")
-            elif job_status == "succeeded":
+
+            get_url = f"http://{self.server_name}:8000/api/v1/jobs/get"
+            get_payload = {"id": job_id}
+
+            job_response = requests.post(url=get_url, json=get_payload, headers=self.headers)
+            job_status = job_response.json().get("job").get("status")  # update job status
+            if job_status == "succeeded":
                 logging.info(f"Job {job_id} has ran successfully.")
+            elif job_status in self.FAILED_JOB_STATUS:
+                raise Exception(f"Job {job_id} has failed. Job status: {job_status}. Error message: {job_response.text}.")
