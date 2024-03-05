744ca934e9ff33ab9d031e4a5c8354961f86fcf7
Oz <ozair.x.ahmed@gmail.com>
2024-03-04 21:27:13 -0500

    Make minor modifications to 2 report models

--- transform/warehouse/models/reports/top_10_customers.sql
+++ transform/warehouse/models/reports/top_10_customers.sql
@@ -1,4 +1,4 @@
-with top_cust_pmnt as (
+with top_cust_by_pmnt as (
 	select
 		p.customer_id,
 		sum(p.amount) as total_payment
@@ -13,7 +13,7 @@ select
 	concat_ws(' ', c.first_name, c.last_name) as customer,
 	c.store_id,
 	tcp.total_payment
-from top_cust_pmnt tcp
+from top_cust_by_pmnt tcp
 join {{ ref('customer') }} c on
 	tcp.customer_id = c.customer_id
 order by 4 desc
